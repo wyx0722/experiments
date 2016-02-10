692b74dab6ce3ec9a276cd493e6e1fde39ec6071
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-02-10 19:10:01 +0900

    VGG-19

--- deep_features_extraction/extract_VGG19_rawFrames_UCF50_1.py
+++ deep_features_extraction/extract_VGG19_rawFrames_UCF50_1.py
@@ -30,7 +30,7 @@ transformer.set_mean('data', np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_
 transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
 transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
 
-net.blobs['data'].reshape(1,3,227,227)
+#net.blobs['data'].reshape(1,3,227,227)
 
 #create a list with all the vidos from the dataset
 with open('/home/ionut/Data/UCF50_tvL1_OpticalFlow/matlabPathsUCF50.txt') as f:
