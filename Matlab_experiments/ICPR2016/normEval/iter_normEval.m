78a67b6b302850188575c417515de7d583712c80
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-03-26 00:06:22 +0900

    exp

--- Matlab_experiments/ICPR2016/normEval/iter_normEval.m
+++ Matlab_experiments/ICPR2016/normEval/iter_normEval.m
@@ -10,7 +10,7 @@ nPar=10;
 
 for i=1:length(norm)
     i
-    normEval_VLAD(func, mediaType, layer, network, norm(i), alpha(i), nCl, pcaD, bPathFeatures, nPar)
+    normEval_VLAD(func, mediaType, layer, network, norm{i}, alpha(i), nCl, pcaD, bPathFeatures, nPar)
 
 end
 
