cc9cf6674f6923aa1d383901a25ae720e3911306
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-03-30 19:40:32 +0900

    exp

---
+++ Matlab_experiments/ICPR2016/clustersEval/iter_evalClustersNum.m
@@ -0,0 +1,16 @@
+function [  ] = iter_evalClustersNum( bPathFeatures, network, clustesDim )
+
+
+func=@FEVid_deepFeatures;
+mediaType='DeepF';
+layer='pool5';
+norm='ROOTSIFT';
+dimPCA=256;
+nPar=5;
+
+for i=1:length(clustesDim)
+    i
+    evalPCAdim_VLAD(func, mediaType, layer, network, norm, clustesDim(i), dimPCA, bPathFeatures, nPar)
+
+end
+
