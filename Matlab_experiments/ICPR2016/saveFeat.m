424075654472061f68a910a1a16f50d3ea1fdce8
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-04-06 18:02:53 +0900

    exp

---
+++ Matlab_experiments/ICPR2016/saveFeat.m
@@ -0,0 +1,17 @@
+
+
+
+ saveName2 = ['/home/ionut/Data/results/ICPR2016_rezults/' 'videoRep/' 'encoding/' DescParam2Name(descParam) '_vladNoMean_.mat']
+ save(saveName2, '-v7.3', 'vladNoMean');
+
+ saveName2 = ['/home/ionut/Data/results/ICPR2016_rezults/' 'videoRep/' 'encoding/' DescParam2Name(descParam) '_maxEncode_.mat']
+ save(saveName2, '-v7.3', 'maxEncode');
+
+saveName2 = ['/home/ionut/Data/results/ICPR2016_rezults/' 'videoRep/' 'encoding/' DescParam2Name(descParam) '_avgEncode_.mat']
+ save(saveName2, '-v7.3', 'avgEncode');
+
+saveName2 = ['/home/ionut/Data/results/ICPR2016_rezults/' 'videoRep/' 'encoding/' DescParam2Name(descParam) '_fisherVectors_.mat']
+ save(saveName2, '-v7.3', 'fisherVectors');
+
+
+
