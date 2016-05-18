8db0fa2df75c1c6483d3aaf316be84fd85febf08
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-05-18 23:04:23 +0200

    exp

---
+++ Matlab_experiments/ACCV2016/UCF50/normPNL2.m
@@ -0,0 +1,70 @@
+
+
+
+
+%% Do classification
+
+nEncoding=4;
+allDist=cell(1, nEncoding);
+
+temp=NormalizeRowsUnit(PowerNormalization(vladNoMean, alpha));
+allDist{1}=temp * temp';
+
+temp=NormalizeRowsUnit(PowerNormalization(vladMean, alpha));
+allDist{2}=temp * temp';
+
+temp=NormalizeRowsUnit(PowerNormalization(stdDiff, alpha));
+allDist{3}=temp * temp';
+
+temp=NormalizeRowsUnit(PowerNormalization(stdDiffMean, alpha));
+allDist{4}=temp * temp';
+
+clear temp
+
+
+all_clfsOut=cell(1,nEncoding);
+all_accuracy=cell(1,nEncoding);
+
+cRange = 100;
+nReps = 1;
+nFolds = 3;
+
+parpool(13);
+for k=1:nEncoding
+
+% 
+% Leave-one-group-out cross-validation
+parfor i=1:max(groups)
+    testI = groups == i;
+    trainI = ~testI;
+    trainDist = allDist{k}(trainI, trainI);
+    testDist = allDist{k}(testI, trainI);
+    trainLabs = labs(trainI,:);
+    testLabs = labs(testI, :);
+    
+    [~, clfsOut{i}] = SvmPKOpt(trainDist, testDist, trainLabs, testLabs, cRange, nReps, nFolds);
+    accuracy{i} = ClassificationAccuracy(clfsOut{i}, testLabs);
+    fprintf('%d: accuracy: %.3f\n', i, mean(accuracy{i}));
+end
+
+all_clfsOut{k}=clfsOut;
+all_accuracy{k}=accuracy;
+
+k
+perGroupAccuracy = mean(cat(2, accuracy{:}))'
+
+mean(perGroupAccuracy)
+
+end
+
+delete(gcp('nocreate'))
+
+
+% saveName = ['/home/ionut/Data/results/ICPR2016_rezults/' 'clfsOut/' 'encoding/'  DescParam2Name(descParam) '_vladNoMean_maxEncode_fisherVectors_.mat'];
+% save(saveName, '-v7.3', 'descParam', 'all_clfsOut', 'all_accuracy');
+% 
+%  saveName2 = ['/home/ionut/Data/results/ICPR2016_rezults/' 'videoRep/' 'encoding/' DescParam2Name(descParam) '_vladNoMean_maxEncode_fisherVectors_.mat'];
+%  save(saveName2, '-v7.3', 'vladNoMean', 'maxEncode', 'fisherVectors');
+%  
+ 
+
