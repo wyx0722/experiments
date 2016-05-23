67b0567b49a925fd29fc9bc2b11638d5b43516f7
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-05-23 02:10:34 +0200

    exp

--- Matlab_experiments/ACCV2016/getRepresentationMultiClusters.m
+++ Matlab_experiments/ACCV2016/getRepresentationMultiClusters.m
@@ -5,13 +5,23 @@ representation=[];
 distance=distmj(desc, bovwCluster.vocabulary);
 [~, assign]=min(distance, [], 2);
 
+if strfind(func2str(func), 'stdDiff')>0
+    stdDiff=1;
+else
+    stdDiff=0;
+end
 
 for i=1:length(cell_smallCls)
     
     assigned=(assign==i);
     
     if sum(assigned)>0
-        rep=func(desc(assigned, :), cell_smallCls{i}.vocabulary);
+        if stdDiff==1
+            rep=func(desc(assigned, :), cell_smallCls{i}.vocabulary, cell_smallCls{i}.st_d);
+        else
+            rep=func(desc(assigned, :), cell_smallCls{i}.vocabulary);
+        end 
+        
     else
         rep=zeros(1, size(cell_smallCls{i}.vocabulary,1)*size(cell_smallCls{i}.vocabulary,2));
     end
