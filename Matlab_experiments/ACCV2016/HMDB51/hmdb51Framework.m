787b0ef51efaa66475b3c6660e1c80f37c8b1d69
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-05-23 21:37:04 +0200

    exp

--- Matlab_experiments/ACCV2016/HMDB51/hmdb51Framework.m
+++ Matlab_experiments/ACCV2016/HMDB51/hmdb51Framework.m
@@ -1,4 +1,4 @@
-function []=hmdb51Framework(descParam, pathFeat )
+function [all_accuracy, all_clfsOut ]=hmdb51Framework(descParam, pathFeat )
 global DATAopts;
 DATAopts = HMDB51Init;
 
@@ -89,7 +89,6 @@ parfor i=1:length(trainTestSetPathFeatures)
     vladNoMean(i, :)=VLAD_1(desc, orgCluster.vocabulary);
     stdDiff(i, :)=stdDiff_VLAD(desc, orgCluster.vocabulary, orgCluster.st_d);
     
-    
     multiVLAD(i, :)=getRepresentationMultiClusters(desc,bovwCluster, cell_smallCls, @VLAD_1);
     multiStdDiff(i, :)=getRepresentationMultiClusters(desc,bovwCluster, cell_smallCls, @stdDiff_VLAD);
 
@@ -170,26 +169,19 @@ end
 
 delete(gcp('nocreate'))
 
+
 acc1=mean(all_accuracy{1})
 acc2=mean(all_accuracy{2})
 acc3=mean(all_accuracy{3})
 acc4=mean(all_accuracy{4})
+acc5=mean(all_accuracy{5})
+acc6=mean(all_accuracy{6})
+acc7=mean(all_accuracy{7})
+acc8=mean(all_accuracy{8})
 
 
 
-% fileName=sprintf('/home/ionut/experiments/Matlab_experiments/ACMMM2016/results/results_HMDB51_Features%s_Layer%s_Network%s_PCAdim%d_clusters%d_norm%s__VLAD.txt', descParam.MediaType,descParam.Layer, descParam.net,descParam.pcaDim, descParam.numClusters, descParam.Normalisation); 
-% fileID=fopen(fileName, 'a');
-% fprintf(fileID, 'Dataset and Split: %s --> vladNoMean acc= %.4f   maxEncode acc= %.4f  fisherVectors acc= %.4f \r\n', descParam.Dataset, acc1,acc2, acc3 );
-% fclose(fileID);
-% 
-% 
-% saveName = ['/home/ionut/Data/results/ICPR2016_rezults/' 'clfsOut/' 'encoding/'  DescParam2Name(descParam) '_vladNoMean_maxEncode_fisherVectors_.mat'];
-% save(saveName, '-v7.3', 'descParam', 'all_clfsOut', 'all_accuracy');
-% 
-%  saveName2 = ['/home/ionut/Data/results/ICPR2016_rezults/' 'videoRep/' 'encoding/' DescParam2Name(descParam) '_vladNoMean_maxEncode_fisherVectors_.mat'];
-%  save(saveName2, '-v7.3', 'vladNoMean', 'maxEncode', 'fisherVectors');
-%  
-% 
-%  
-% 
-% end
+saveName2 = ['/home/ionut/Data/results/' 'accv2016/' DescParam2Name(descParam) '.mat']
+save(saveName2, '-v7.3', 'vladNoMean', 'stdDiff', 'multiVLAD', 'multiStdDiff');
+ 
+ end
