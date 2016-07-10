60d17ccf06cd3574c0d1b4392e69b14d1cbfe46d
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-07-11 00:09:23 +0200

    exp

---
+++ Matlab_experiments/ACCV2016/sp_clustering/hmdb51/saveF.m
@@ -0,0 +1,4 @@
+saveName2 = ['/home/ionut/asustor_ionut_2/Data/results/' 'mmm2016/' DescParam2Name(descParam) '.mat']
+save(saveName2, '-v7.3', 'v256', 'v320', 'spV8', 'spV32', 'spV64', 'spV256', 'descParam');
+
+exit
