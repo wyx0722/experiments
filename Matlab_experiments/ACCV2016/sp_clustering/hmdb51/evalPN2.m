9b95090287f78991ce76490f89f9d42b69467d7e
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-07-15 23:46:58 +0200

    exp

---
+++ Matlab_experiments/ACCV2016/sp_clustering/hmdb51/evalPN2.m
@@ -0,0 +1,62 @@
+
+
+%% Do classification
+nEncoding=8
+allDist=cell(1, nEncoding);
+alpha=[0.6:0.1:0.9]
+
+
+for i=1:length(alpha)
+temp=NormalizeRowsUnit(PowerNormalization(v256, alpha(i)));
+allDist{i}=temp * temp';
+
+temp=NormalizeRowsUnit(PowerNormalization(spV64, alpha(i)));
+allDist{4+i}=temp * temp';
+
+end
+
+clear temp
+
+all_clfsOut=cell(1,nEncoding);
+all_accuracy=cell(1,nEncoding);
+
+cRange = 100;
+nReps = 1;
+nFolds = 3;
+
+
+parpool(2);
+parfor k=1:nEncoding
+    k
+    for i=1:3
+        
+        trainI = splits(:,i) == 1;
+        testI  = splits(:,i) == 2;
+        trainLabs = labs(trainI,:);
+        testLabs = labs(testI,:);
+        
+        trainDist = allDist{k}(trainI, trainI);
+        testDist = allDist{k}(testI, trainI);
+        
+
+        [~, clfsOut] = SvmPKOpt(trainDist, testDist, trainLabs, testLabs, cRange, nReps, nFolds);
+        accuracy = ClassificationAccuracy(clfsOut, testLabs);
+        fprintf('accuracy: %.3f\n', accuracy);
+
+        all_clfsOut{k,i}=clfsOut;
+        all_accuracy{k,i}=accuracy;
+    end
+end
+
+delete(gcp('nocreate'))
+
+
+finalAcc=zeros(1,nEncoding);
+for j=1:nEncoding
+    mean_all_clfsOut{j}=(all_clfsOut{j,1} + all_clfsOut{j,2} + all_clfsOut{j,3})./3;
+    mean_all_accuracy{j}=(all_accuracy{j,1} + all_accuracy{j,2} + all_accuracy{j,3})./3;
+    
+    finalAcc(j)=mean(mean_all_accuracy{j});
+    fprintf('Encoding %d --> MAcc: %.3f \n', j, finalAcc(j));
+end
+
