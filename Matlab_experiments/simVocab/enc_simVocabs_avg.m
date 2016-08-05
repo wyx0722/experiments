4f939df4f38fe4d6c69fd091e718b406b0427e7e
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-08-05 18:26:07 +0200

    exp

--- Matlab_experiments/simVocab/enc_simVocabs_avg.m
+++ Matlab_experiments/simVocab/enc_simVocabs_avg.m
@@ -1,7 +1,7 @@
 function [ enc_vect ] = enc_simVocabs_avg( desc, vocabsClass)
 
 k=size(vocabsClass{1}, 1);
-enc_vect=zeros(1,length(vocabsClass)*k);
+enc_vect=cell(1,length(vocabsClass));
 
 for v=1:length(vocabsClass)
     distance=distmj(desc, vocabsClass{v});
@@ -15,17 +15,52 @@ for v=1:length(vocabsClass)
     end
     
     %get the average distance for each cluster
+    k=size(vocabsClass{v}, 1);
+    singleVoc=zeros(1, k);
     for i=1:k
         assigned=(assign==i);
         if(sum(assigned)>0)
-            poz=v*k-k+i;
-           enc_vect(poz)=mean(distAssign(assigned)); 
+           singleVoc(i)=mean(distAssign(assigned)); 
         end               
     end
     
+    enc_vect{v}=singleVoc;
+    
+end
+
+enc_vect=cat(2, enc_vect{:});
+
 end
 
 
-
-end
-
+%the old one
+% function [ enc_vect ] = enc_simVocabs_avg( desc, vocabsClass)
+% 
+% k=size(vocabsClass{1}, 1);
+% enc_vect=zeros(1,length(vocabsClass)*k);
+% 
+% for v=1:length(vocabsClass)
+%     distance=distmj(desc, vocabsClass{v});
+%     [distAssign, assign]=min(distance, [], 2);
+%     
+%     %check if all the vocabularies are the same size
+%     if size(vocabsClass{v}, 1)~=k
+%         fprintf('Size vacab%d: %d and size vocab%d: %d \n', 1, k, v,size(vocabsClass{v}, 1));
+%         warning('The size of the vocabularies are not equal!!!!!!!!');
+%         keyboard
+%     end
+%     
+%     %get the average distance for each cluster
+%     for i=1:k
+%         assigned=(assign==i);
+%         if(sum(assigned)>0)
+%             poz=v*k-k+i;
+%            enc_vect(poz)=mean(distAssign(assigned)); 
+%         end               
+%     end
+%     
+% end
+% 
+% 
+% 
+% end
