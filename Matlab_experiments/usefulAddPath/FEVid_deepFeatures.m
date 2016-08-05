66b2bda1962fe92b9da87916470b6649072747f6
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-08-05 11:07:06 +0200

    exp

---
+++ Matlab_experiments/usefulAddPath/FEVid_deepFeatures.m
@@ -0,0 +1,16 @@
+function [desc, info, descParam] = FEVid_deepFeatures(featurePath, descParam)
+
+info.row=0;
+info.col=0;
+info.depth=0;
+info.descSize=0;
+info.vidSize=0;
+info.imSize=0;
+info.mediaName=featurePath;
+
+if ~isfield(descParam, 'NormFeatureMaps')
+    descParam.NormFeatureMaps='None';
+end
+
+[desc, spInfo] = maps2features(featurePath, descParam.Layer, descParam.NormFeatureMaps);
+info.spInfo=spInfo;
