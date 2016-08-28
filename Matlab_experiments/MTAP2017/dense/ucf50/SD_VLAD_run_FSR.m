c5d8857e22acdc432bf31e84ca31db370420877d
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-08-28 09:39:05 +0200

    exp

---
+++ Matlab_experiments/MTAP2017/dense/ucf50/SD_VLAD_run_FSR.m
@@ -0,0 +1,24 @@
+
+
+typeFeature=cell(1,5);
+typeFeature{1}=@FEVidHogDense
+typeFeature{2}=@FEVidHofDense
+typeFeature{3}=@FEVidHmgDense
+typeFeature{4}=@FEVidMBHxDense
+typeFeature{5}=@FEVidMBHyDense
+
+all_accuracy=cell(1,5);
+all_clfsOut=cell(1,5);
+
+
+normStrategy='ROOTSIFT';
+d=72;
+cl=256;
+%fsr=?;
+nPar=5;
+alpha=0.1;
+
+
+for i=1:length(typeFeature)
+    [all_accuracy{i}, all_clfsOut{i}]  = SD_VLADFramework(typeFeature{i}, normStrategy, d, cl, fsr, nPar, alpha);
+end
