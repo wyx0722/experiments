f479130430a2bd3066a10c3d100deac272f8b191
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-08-28 09:25:38 +0200

    exp

--- Matlab_experiments/MTAP2017/dense/ucf50/FVrun_mbh_x_y.m
+++ Matlab_experiments/MTAP2017/dense/ucf50/FVrun_mbh_x_y.m
@@ -1,5 +1,4 @@
 
-
 typeFeature=cell(1,2);
 typeFeature{1}=@FEVidMBHxDense
 typeFeature{2}=@FEVidMBHyDense
@@ -17,6 +16,29 @@ nPar=5;
 alpha=0.5;
 
 
-for i=1:length(typeFeature)
-    [all_accuracy{i}, all_clfsOut{i}]  = FisherFramework(typeFeature{i}, normStrategy, d, cl, fsr, nPar, alpha);
-end
+
+    [all_accuracy{2}, all_clfsOut{2}]  = FisherFramework(typeFeature{2}, normStrategy, d, cl, fsr, nPar, alpha);
+
+
+
+
+% typeFeature=cell(1,2);
+% typeFeature{1}=@FEVidMBHxDense
+% typeFeature{2}=@FEVidMBHyDense
+% 
+% 
+% all_accuracy=cell(1,2);
+% all_clfsOut=cell(1,2);
+% 
+% 
+% normStrategy='ROOTSIFT';
+% d=72;
+% cl=256;
+% fsr=1;
+% nPar=5;
+% alpha=0.5;
+% 
+% 
+% for i=1:length(typeFeature)
+%     [all_accuracy{i}, all_clfsOut{i}]  = FisherFramework(typeFeature{i}, normStrategy, d, cl, fsr, nPar, alpha);
+% end
