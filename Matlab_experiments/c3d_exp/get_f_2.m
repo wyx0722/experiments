4ee6c7488e5683dd4e1bd94f7d1d135b5cc16946
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-09-27 16:05:13 +0200

    exp

---
+++ Matlab_experiments/c3d_exp/get_f_2.m
@@ -0,0 +1,32 @@
+function [ allFeatures ] = get_f_2( pathFeatures, layer )
+
+
+if ~isempty(strfind(layer, 'pool4'))
+    rows=4;
+    cols=4;  
+    channels=512;
+end
+
+
+loadName=sprintf('%s%s.mat',pathFeatures, layer);
+
+load(loadName)
+
+
+allFeatures=zeros(rows*cols*length(allFeatures), channels);
+p=1;
+
+for i=1:length(allFeatures)
+    
+    
+    for m=1:size(blob, 4)
+        for n=size(blob, 5)
+           allFeatures(p, :)=allFeatures{i}(1, :, 1, m, n);
+           p=p+1;
+        end
+    end
+end
+
+
+end
+
