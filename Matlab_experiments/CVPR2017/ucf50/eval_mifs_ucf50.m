5dd3a0a2ee4788a0b43d38e6976a4bc922ca1050
Ionut <Ionut@Ionuts-MacBook-Air.local>
2016-10-19 22:50:11 +0200

    exp

--- Matlab_experiments/CVPR2017/ucf50/eval_mifs_ucf50.m
+++ Matlab_experiments/CVPR2017/ucf50/eval_mifs_ucf50.m
@@ -19,31 +19,93 @@
 % all_data=cat(1, cell_features{:});
 
 
-load([ path_features 'imdb.mat']);
+% load([ path_features 'imdb.mat']);
+% 
+% 
+% 
+% global DATAopts;
+% DATAopts = UCFInit;
+% 
+% 
+% [allVids, labs, groups] = GetVideosPlusLabels('Full');
+% 
+% representation=zeros(length(allVids), size(all_data, 2));
+% 
+% for i=1:length(allVids)
+%     if mod(i, 100)==0
+%         fprintf('%d ', i);
+%     end
+%     
+%     for j=1:(length(images.name))
+%         
+%         if ~isempty(strfind([meta.classes{images.class(j)} '/' images.name{j}(1:end-4)], allVids{i}))
+%             representation(i, :)=all_data(j, :);
+%             continue
+%         end
+%         
+%     end 
+%     
+%     
+% end
 
+nPar=5;
 
+nEncoding=2;
+allDist=cell(1, nEncoding);
 
-global DATAopts;
-DATAopts = UCFInit;
+temp=NormalizeRowsUnit(representation);
+allDist{1}=temp * temp';
 
+temp=NormalizeRowsUnit(PowerNormalization(representation, alpha));
+allDist{2}=temp * temp';
 
-[allVids, labs, groups] = GetVideosPlusLabels('Full');
+all_clfsOut=cell(1,nEncoding);
+all_accuracy=cell(1,nEncoding);
 
-representation=zeros(length(allVids), size(all_data, 2));
+cRange = 100;
+nReps = 1;
+nFolds = 3;
 
-for i=1:length(allVids)
-    if mod(i, 100)==0
-        fprintf('%d ', i);
-    end
+parpool(nPar);
+for k=1:nEncoding
+k
+% 
+% Leave-one-group-out cross-validation
+parfor i=1:max(groups)
+    testI = groups == i;
+    trainI = ~testI;
+    trainDist = allDist{k}(trainI, trainI);
+    testDist = allDist{k}(testI, trainI);
+    trainLabs = labs(trainI,:);
+    testLabs = labs(testI, :);
     
-    for j=1:(length(images.name))
-        
-        if ~isempty(strfind([meta.classes{images.class(j)} '/' images.name{j}(1:end-4)], allVids{i}))
-            representation(i, :)=all_data(j, :);
-            continue
-        end
-        
-    end 
-    
+    [~, clfsOut{i}] = SvmPKOpt(trainDist, testDist, trainLabs, testLabs, cRange, nReps, nFolds);
+    accuracy{i} = ClassificationAccuracy(clfsOut{i}, testLabs);
+    fprintf('%d: accuracy: %.3f\n', i, mean(accuracy{i}));
+end
+
+all_clfsOut{k}=clfsOut;
+all_accuracy{k}=accuracy;
+
+fprintf('Accuracy for encoding %d: %.3f\n',k, mean(mean(cat(2, accuracy{:}))'));
+
+end
+
+delete(gcp('nocreate'))
+
+clear allDist
+
+
+
+finalAcc=zeros(1,nEncoding);
+for j=1:nEncoding
+
+    finalAcc(j)=mean(mean(cat(2, all_accuracy{j}{:}), 2));
+    fprintf('%.3f\n', finalAcc(j));
+
     
 end
+
+ 
+ 
+ 
